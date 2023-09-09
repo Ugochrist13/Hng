dce4a477511c0522d8550f29c11cd85c7ee6a5aa
Christian <christain@afrilish.com>
2023-09-09 20:02:05 +0100

    first push

---
+++ stage-one/index.js
@@ -0,0 +1,18 @@
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDate = new Date();
+const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+const currentUTCTime = Date.now();
+
+document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent =
+  currentDay;
+document.querySelector('[data-testid="currentUTCTime"]').textContent =
+  currentUTCTime;
