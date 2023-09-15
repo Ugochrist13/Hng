8d4233b159f5d80d51636b727604234a6f1c1681
Christian <christain@afrilish.com>
2023-09-15 14:48:29 +0100

    update on sidebar

--- GentMovieApp/src/components/MovieDetail.tsx
+++ GentMovieApp/src/components/MovieDetail.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 interface MovieDetailProps {
   title: string;
@@ -15,29 +16,52 @@ const MovieDetail: React.FC<MovieDetailProps> = ({
   overview,
   backdrop_path,
 }) => {
-  
   const backdropBaseUrl = "https://image.tmdb.org/t/p/original";
 
   return (
-    <div className="w-full h-screen p-2" data-testid="movie-detail">
-      <div
-        style={{
-          backgroundImage: `linear-gradient(
+    <div className="w-full flex items-center justify-between h-screen py-2" data-testid="movie-detail">
+      <div className="h-screen fixed">
+      <Sidebar  />
+      </div>
+      <div className="w-[90%] h-screen md:ml-36">
+        <div
+          style={{
+            backgroundImage: `linear-gradient(
         to bottom,
         rgba(59, 67, 50, 0.4),
         rgba(52, 73, 3, 0.079)
       ), url(${backdropBaseUrl}${backdrop_path})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-        className="w-full h-[80%]"
-      ></div>
-      <div className="w-full flex justify-center items-center gap-x-20 mt-2">
-        <h2 data-testid="movie-title" className="text-primary hover:text-hover">{title}</h2>
-        <p data-testid="movie-release-date" className="text-primary hover:text-hover" >{release_date}</p>
-        <p data-testid="movie-runtime" className="text-primary hover:text-hover" >{`${runtime} minutes`}</p>
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="w-full h-[80%]"
+        ></div>
+        <div className="w-full flex justify-center items-center gap-x-20 mt-2">
+          <h2
+            data-testid="movie-title"
+            className="text-primary hover:text-hover"
+          >
+            {title}
+          </h2>
+          <p
+            data-testid="movie-release-date"
+            className="text-primary hover:text-hover"
+          >
+            {release_date}
+          </p>
+          <p
+            data-testid="movie-runtime"
+            className="text-primary hover:text-hover"
+          >{`${runtime} minutes`}</p>
+        </div>
+        <p
+          data-testid="movie-overview"
+          className="p-4 text-sm font-semibold text-justify text "
+        >
+          {" "}
+          <span className="text-primary">Overview:</span> {overview}
+        </p>
       </div>
-      <p data-testid="movie-overview" className="p-4 text-sm font-semibold text-justify text "> <span className="text-primary">Overview:</span> {overview}</p>
     </div>
   );
 };
---
+++ GentMovieApp/src/components/Sidebar.tsx
@@ -0,0 +1,164 @@
+import { useState, useEffect } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
+import logo from "../assets/logo.svg";
+import tv from "../assets/tv.svg";
+import Home from "../assets/home.svg";
+import logout from "../assets/logout.svg";
+import movie from "../assets/movie.svg";
+import calender from "../assets/calender.svg";
+import "./sidebar.css";
+
+const Sidebar = () => {
+  const [userTab, setUserTab] = useState("Home");
+  const [userSidebar, setUserSidebar] = useState(false);
+
+  useEffect(() => {
+    const savedTab = localStorage.getItem("currentUserTab");
+    if (savedTab) {
+      setUserTab(savedTab);
+    }
+  }, []);
+
+  const handleTabClick = (tabName: string) => {
+    setUserTab(tabName);
+    setUserSidebar(false);
+    localStorage.setItem("currentUserTab", tabName);
+  };
+
+  return (
+    <div
+      className={`bg-white border rounded-lg border-gray-700 fixed z-20 lg:min-h-screen flex flex-col justify-between md:justify-between md:flex-col lg:justify-start items-center lg:flex-col w-full lg:w-[11%] lg:mr-4`}
+    >
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-x-2 sidebar-logo p-3">
+          <img className="mx-auto w-8" src={logo} alt="Logo" />{" "}
+          <p className="text-xs">MovieBox</p>
+        </div>
+        <button
+          className="lg:hidden text-2xl py-2 text-grayText rounded-md outline-none focus:border-gray-400"
+          onClick={() => setUserSidebar(!userSidebar)}
+        >
+          {userSidebar ? (
+            <GrClose className="mr-2" />
+          ) : (
+            <GiHamburgerMenu className="mr-2" />
+          )}
+        </button>
+      </div>
+      <ul
+        className={`absolute pl-2  mx-0 left-0 top-[3.4rem] lg:top-0 lg:-mt-1 lg:static ${
+          userSidebar
+            ? "left-0 w-1/2 md:w-[25%] min-h-screen  lg:w-auto lg:h-auto"
+            : "left-[-55rem] min-h-screen lg:h-auto lg:left-0"
+        } sidebar-ul mt-12 transition-all duration-1000 ease-in`}
+      >
+        <li
+          className={`text-sm my-5 p-2 text-black ${
+            userTab === "Home" ? "active" : ""
+          }`}
+        >
+          <button
+            className="flex items-center  space-x-4"
+            onClick={() => {
+              setUserTab("Home");
+              setUserSidebar(false);
+            }}
+          >
+            <span className="icon">
+              <img src={Home} alt="" />
+            </span>{" "}
+            <span className="">Home</span>
+          </button>
+        </li>
+
+        <li
+          className={`text-sm my-5 p-2  text-black ${
+            userTab === "Movies" ? "active" : ""
+          }`}
+        >
+          <button
+            className="flex items-center space-x-4 "
+            onClick={() => {
+              handleTabClick("Movies");
+            }}
+          >
+            <span className="icon">
+              <img src={movie} alt="" />
+            </span>{" "}
+            <span>Movies</span>
+          </button>
+        </li>
+        <li
+          className={`text-sm my-5 p-2 text-black ${
+            userTab === "TvSeries" ? "active" : ""
+          }`}
+        >
+          <button
+            className="flex items-center space-x-4"
+            onClick={() => {
+              handleTabClick("TvSeries");
+            }}
+          >
+            <span className="icon">
+              <img src={tv} alt="" />
+            </span>{" "}
+            <span>Tv Series</span>
+          </button>
+        </li>
+        <li
+          className={`text-sm my-5 p-2 text-black ${
+            userTab === "Upcoming" ? "active" : ""
+          }`}
+        >
+          <button
+            className="flex items-center space-x-4"
+            onClick={() => {
+              handleTabClick("Upcoming");
+            }}
+          >
+            <span className="icon">
+              <img src={calender} alt="" />
+            </span>{" "}
+            <span>Upcoming</span>
+          </button>
+        </li>
+
+        <li className="mobile-app-image pt-2 p-2">
+          <>
+            <div className="border border-pink-200 p-3 w-3full mx-auto rounded-lg">
+              <p className="text-sm font-medium py-2">
+                Play movie quizes and earn movie tickets
+              </p>
+
+              <p
+                style={{ fontSize: "10px" }}
+                className="w-full text-gray-400 mb-3"
+              >
+                50k are playing right now
+              </p>
+              <button className="py-1 bg-pink-100 px-3 rounded-xl mx-auto text-center text-pink-400 text-xs">
+                Start Playing
+              </button>
+            </div>
+          </>
+        </li>
+        <li className="text-sm mb-5 p-3 z-10 text-black">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            className="flex items-center gap-2 py-2"
+          >
+            <span className="icon">
+              <img src={logout} alt="" />
+            </span>{" "}
+            <span>Logout</span>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
